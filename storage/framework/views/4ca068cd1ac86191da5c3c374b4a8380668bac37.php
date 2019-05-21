ae4e6d959816c97360a0a02263cb7d6f7dadde53
Daniel <daniel-santos2000@live.com>
2019-05-21 11:35:10 -0300

    ptojeto laravel

--- storage/framework/views/4ca068cd1ac86191da5c3c374b4a8380668bac37.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<?php echo $__env->make('layouts._site._slides', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-
-<div class="container">
-   <?php echo $__env->make('layouts._site._lista_imoveis', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-</div>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.site', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Estudos\laravel\webSite_completo\SiteDinamico\resources\views/site/home.blade.php ENDPATH**/ ?>
